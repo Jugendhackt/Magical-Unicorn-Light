4e005b18703406b76d958884e4d226aada856e8c
ebosseck <eric.bossecker@gmail.com>
2016-10-15 22:17:07 +0200

    Added DMX example

--- Magical-Unicorn-Light/src/org/jugendhackt/magicalunicornlight/Main.java
+++ Magical-Unicorn-Light/src/org/jugendhackt/magicalunicornlight/Main.java
@@ -10,8 +10,46 @@ import org.jugendhackt.magicalunicornlight.frames.MagicFrame;
 public class Main {
 
 	public static void main(String[] args) {
-		testDMX2 ();
+		test ();
+	}
+	
+	public static void test () {
+		Random rand = new Random();
 		
+		MagicSender output = new MagicSender ();
+		BufferedDMXSender dmxOutput = new BufferedDMXSender ();
+		try {
+			output.openPort();
+			output.setAddress("100.100.218.77");
+			output.setPort(1337);
+			
+			dmxOutput.openPort();
+			dmxOutput.setAddress("127.0.0.1");
+			dmxOutput.setPort(5555);
+			DMXFrame frame = new DMXFrame ();
+//			frame.setChannelValue(101, 255);
+//			int i = 0;
+			while (true) {
+				frame.setChannelValueDouble(1, rand.nextDouble());
+				frame.setChannelValueDouble(2, rand.nextDouble());
+				frame.setChannelValueDouble(3, rand.nextDouble());
+				output.sendFrame(frame);
+				
+				frame.setChannelValueByte(101, frame.getChannelValue(1));
+				frame.setChannelValueByte(102, frame.getChannelValue(2));
+				frame.setChannelValueByte(103, frame.getChannelValue(3));
+				
+				dmxOutput.sendFrame(frame);
+				
+				Thread.sleep(500);
+				System.out.println("Sending to " + output.getAddress().toString() + ":" + output.getPort());
+//				i++;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			output.closePort();
+		}
 	}
 	
 	public static void testMagicSender () {
@@ -22,12 +60,20 @@ public class Main {
 			output.openPort();
 			output.setAddress("100.100.246.62");
 			output.setPort(1337);
-			MagicFrame frame = new MagicFrame ();
+			DMXFrame frame = new DMXFrame ();
 //			frame.setChannelValue(101, 255);
 //			int i = 0;
 			while (true) {
-				frame.setColor(rand.nextDouble(), rand.nextDouble(), rand.nextDouble());
+				frame.setChannelValueDouble(1, rand.nextDouble());
+				frame.setChannelValueDouble(2, rand.nextDouble());
+				frame.setChannelValueDouble(3, rand.nextDouble());
 				output.sendFrame(frame);
+				
+				frame.setChannelValueDouble(101, rand.nextDouble());
+				frame.setChannelValueDouble(102, rand.nextDouble());
+				frame.setChannelValueDouble(103, rand.nextDouble());
+//				dmxOutput.sendFrame(frame);
+				
 				Thread.sleep(500);
 				System.out.println("Sending to " + output.getAddress().toString() + ":" + output.getPort());
 //				i++;
