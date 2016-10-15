dc0024fbc39b0181d43b86218d12202498e2358e
Ulf-vom-Mond <noahrullmann@gmail.com>
2016-10-15 12:19:10 +0200

    MusikImport, jetzt auch Pommes im Angebot

---
+++ Magical-Unicorn-Light/src/org/jugendhackt/magicalunicornlight/MusikImport.java
@@ -0,0 +1,30 @@
+package org.jugendhackt.magicalunicornlight;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+
+public class MusikImport {
+	static void musikImport() {
+	try {
+	    AudioInputStream input = AudioSystem.getAudioInputStream(new File("/home/jh/jh2016/Musik/Everlasting.wav"));
+	    SourceDataLine line = AudioSystem.getSourceDataLine(input.getFormat());
+	    line.open(input.getFormat());
+	    line.start();
+	    byte[] buffer = new byte[1024];
+	    int count;
+	    while((count = input.read(buffer, 0, 1024)) != -1) {
+	        line.write(buffer, 0, count);
+	    }
+	    line.drain();
+	    line.stop();
+	    line.close();
+	} catch(Exception e) {
+	    e.printStackTrace();
+	}
+	}
+	public static void main(String[] args) {
+		musikImport();
+		System.out.println("Ihr Veggie-Burger ist fertig!");
+	}
+}
